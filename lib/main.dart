fa3daf93ce61329a74abecaf56c0922e628d6ae7
Ciaossuuu <rhowelp@gmail.com>
2020-01-12 22:21:19 +0800

    Login-UI :smirk:

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,245 @@
 import 'package:flutter/material.dart';
-import './dialogs.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+      home: MyApp(),
+      debugShowCheckedModeBanner: false,
+    ));
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'ABC',
-        theme: ThemeData(
-         
-          primarySwatch: Colors.blue,
-        ),
-        home: Something());
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isSelected = false;
+
+  void _radio() {
+    setState(() {
+      _isSelected = !_isSelected;
+    });
   }
-}
 
-class Something extends StatefulWidget {
-  Something({Key key}) : super(key: key);
+  Widget radioButton(bool isSelected) => Container(
+        width: 16.0,
+        height: 16.0,
+        padding: EdgeInsets.all(2.0),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(width: 2.0, color: Colors.black),
+        ),
+        child: isSelected
+            ? Container(
+                width: double.infinity,
+                height: double.infinity,
+                decoration:
+                    BoxDecoration(shape: BoxShape.circle, color: Colors.black),
+              )
+            : Container(),
+      );
 
-  @override
-  _SomethingState createState() => _SomethingState();
-}
+  Widget horizontalLine() => Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.0),
+        child: Container(
+          width: ScreenUtil.getInstance().setWidth(120),
+          height: 1.0,
+          color: Colors.black26.withOpacity(.2),
+        ),
+      );
 
-class _SomethingState extends State<Something> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  TextEditingController num1 = new TextEditingController(),
-      num2 = TextEditingController(),
-      num3 = TextEditingController();
-  bool _autoValidate = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-     
-        body: Stack(
-          children: <Widget>[
-            Container(
-            // color: Colors.red,
-                 decoration: BoxDecoration(
-                  image: DecorationImage(
-                     image: AssetImage("assets/bg.jpg"),
-                      fit: BoxFit.cover))
-                      ),
-            Positioned(
-              top: 90,
-              left: 80,
-              child: Container(
-                width: 350,
-                height: 400,
-              
-                  child: Card(
+    ScreenUtil.instance = ScreenUtil.getInstance()..init(context);
+    ScreenUtil.instance =
+        ScreenUtil(width: 750, height: 1334, allowFontScaling: true);
+    return new Scaffold(
+      backgroundColor: Colors.white,
+      resizeToAvoidBottomPadding: true,
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Image.asset(
+                "assets/background.jpg",
+                fit: BoxFit.cover,
+                height: ScreenUtil.getInstance().setHeight(1330),
+                width: ScreenUtil.getInstance().setWidth(800),
+              )
+            ],
+          ),
+          SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.only(left: 28.0, right: 28.0, top: 60.0),
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: ScreenUtil.getInstance().setHeight(180),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0, 40, 0, 90),
+                    width: double.infinity,
+                    height: ScreenUtil.getInstance().setHeight(550),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                            offset: Offset(0.0, 15.0),
+                            blurRadius: 15.0),
+                        BoxShadow(
+                            color: Colors.black12,
+                            offset: Offset(0.0, -10.0),
+                            blurRadius: 10.0),
+                    ]),
                     child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Form(
-                        key: _formKey,
-                        child: Column(
-                          children: <Widget>[
-                            Center(child: Text('Enter Values',style: TextStyle(
-                              fontSize: 15,fontWeight: FontWeight.bold
-                            ),),),
-                            buildForm(num1, 'Number 1'),
-                            buildForm(num2, 'Number 2'),
-                            buildForm(num3, 'Number 3 '),
-                            SizedBox(
-                              height: 25,
+                      padding:
+                          EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text("Login",
+                            style: TextStyle(
+                              fontSize: ScreenUtil.getInstance().setSp(45),
+                              fontFamily: "Poppins-Bold",
+                              letterSpacing: .6),
                             ),
-                            MaterialButton(
-                              color: Colors.red,
-                              child: Text('CLICK TO SEE AVERAGE'),
-                              onPressed: () {
-                                if (_formKey.currentState.validate()) {
-                                  Checking(num1, num2, num3, context);
-                                } else {
-                                  setState(() => _autoValidate = true);
-                                }
-                              },
-                            )
-                          ],
-                        ),
+                          SizedBox(
+                            height: ScreenUtil.getInstance().setHeight(30),
+                          ),
+                          TextField(
+                            decoration: InputDecoration(
+                              hintText: "Username",
+                              hintStyle: TextStyle(
+                                color: Colors.grey, fontSize: 12.0)),
+                          ),
+                          SizedBox(
+                            height: ScreenUtil.getInstance().setHeight(30),
+                          ),
+                          TextField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              hintText: "Password",
+                              hintStyle: TextStyle(
+                                color: Colors.grey, fontSize: 12.0)
+                            ),
+                          ),
+                          SizedBox(
+                            height: ScreenUtil.getInstance().setHeight(35),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              InkWell(
+                                child: Container(
+                                  margin: EdgeInsets.fromLTRB(0, 0, 0, 15),
+                                  width: ScreenUtil.getInstance().setWidth(330),
+                                  height: ScreenUtil.getInstance().setHeight(100),
+                                  decoration: BoxDecoration(
+                                    gradient: LinearGradient(colors: [
+                                      Colors.pink[50],
+                                      Colors.blue[300]
+                                    ]),
+                                    borderRadius: BorderRadius.circular(6.0),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Color(0xFF6078ea).withOpacity(.3),
+                                        offset: Offset(0.0, 8.0),
+                                        blurRadius: 8.0)
+                                    ]),
+                                  child: Material(
+                                    color: Colors.transparent,
+                                    child: InkWell(
+                                        onTap: () {},
+                                        child: Center(
+                                          child: Text("SIGN IN",
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              fontFamily: "Poppins-Bold",
+                                              fontSize: 18,
+                                              letterSpacing: 1.0),
+                                            ),
+                                        )),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  SizedBox(
+                                    width: 12.0,
+                                  ),
+                                  GestureDetector(
+                                    onTap: _radio,
+                                    child: radioButton(_isSelected),
+                                  ),
+                                  SizedBox(
+                                    width: 8.0,
+                                  ),
+                                  Text("Remember me",
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                        fontFamily: "Poppins-Medium")
+                                      ),
+                                ],
+                              ),
+                              Text(
+                                "Forgot Password?",
+                                style: TextStyle(
+                                    color: Colors.blue,
+                                    fontFamily: "Poppins-Medium",
+                                    fontSize:
+                                        ScreenUtil.getInstance().setSp(28)),
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
                     ),
                   ),
-                
+                  SizedBox(
+                    height: ScreenUtil.getInstance().setHeight(40)
+                  ),
+                  SizedBox(
+                    height: ScreenUtil.getInstance().setHeight(40),
+                  ),
+                  SizedBox(
+                    height: ScreenUtil.getInstance().setHeight(40),
+                  ),
+                  SizedBox(
+                    height: ScreenUtil.getInstance().setHeight(30),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "New User? ",
+                        style: TextStyle(fontFamily: "Poppins-Medium"),
+                      ),
+                      InkWell(
+                        onTap: () {},
+                        child: Text("Signup",
+                          style: TextStyle(
+                            color: Color(0xFF5d74e3),
+                            fontFamily: "Poppins-Bold",
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
               ),
             ),
-          ],
-        ));
-  }
-}
-
-TextFormField buildForm(TextEditingController controller, String label) {
-  return TextFormField(
-    controller: controller,
-    keyboardType: TextInputType.number,
-    decoration: InputDecoration(
-
-      icon: Icon(Icons.account_circle),
-      labelText: label,
-    ),
-    validator: (value) {
-      if (value.isEmpty) {
-        return 'Please enter some Value';
-      }
-      return null;
-    },
-    // onSaved: (value) => name = value,
-  );
-}
-
-void Checking(n1, n2, n3, context) {
-  Dialogs dialog = new Dialogs();
-  double number1 = double.parse(n1.text);
-  double number2 = double.parse(n2.text);
-  double number3 = double.parse(n3.text);
-
-  double average = (number1 + number2 + number3) / 3;
-
-  if (average >= 75 && average <= 100) {
-    dialog.success(context, 'Congratulation Pasado');
-  } else {
-    dialog.fail(context, 'Failed');
+          ),
+        ],
+      ),
+    );
   }
 }
