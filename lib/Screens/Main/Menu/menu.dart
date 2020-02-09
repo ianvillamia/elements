f8662f75c40ecc887de2b211ab453e3dfad1577f
ianvillamia <ianvillamia0123@gmail.com>
2020-02-10 00:06:39 +0800

    status bar disabled

--- lib/Screens/Main/Menu/menu.dart
+++ lib/Screens/Main/Menu/menu.dart
@@ -9,7 +9,6 @@ class Menu extends StatefulWidget {
 
   @override
   _MenuState createState() => _MenuState();
-
 }
 
 final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
@@ -18,98 +17,90 @@ class _MenuState extends State<Menu> {
   PageController pageController;
   int current = 0;
   List images = [
-    "assets/learning-module.png",
+    "assets/.png",
     "assets/compound-simulation.jpg",
     "assets/periodic-table.png"
   ];
-  List name = [
-    "Learning Module",
-    "Compound Simulation",
-    "Periodic Table"
-  ];
+  List name = ["Learning Module", "Compound Simulation", "Periodic Table"];
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     pageController = PageController(initialPage: 1, viewportFraction: 0.8);
   }
 
   @override
   Widget build(BuildContext context) => Scaffold(
-      backgroundColor: Colors.white,
-      key: _scaffoldKey,
-      drawer: buildDrawer(),
-      bottomNavigationBar:buildBottomNavigation(),
-      body: Stack(
-        children: <Widget>[   
-          SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 15,
-                ),
-                buildappbar(context, _scaffoldKey),
-                Container(
-                  height: 400, 
-                  child: test()
-                ),
-              ],
+        backgroundColor: Colors.white,
+        key: _scaffoldKey,
+   
+        bottomNavigationBar: buildBottomNavigation(),
+        body: Stack(
+          children: <Widget>[
+            SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 15,),
+                  buildappbar(context, _scaffoldKey),
+                  Container(
+                
+                    child: Container(height: 400, child: test()),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
-      ),
-  );
+          ],
+        ),
+      );
 
-
-  test(){
-   return PageView.builder(
+  test() {
+    return PageView.builder(
       controller: pageController,
       itemCount: images.length,
-      itemBuilder: (context, position){
+      itemBuilder: (context, position) {
         return _carousel(position);
       },
     );
   }
-  _carousel(int index){
+
+  _carousel(int index) {
     return AnimatedBuilder(
-      animation: pageController, 
-      builder: (context, widget){
+      animation: pageController,
+      builder: (context, widget) {
         double value = 1;
-        if(pageController.position.haveDimensions){
+        if (pageController.position.haveDimensions) {
           value = pageController.page - index;
           value = (1 - (value.abs() * 0.3)).clamp(0.0, 1.0);
         }
-        return Center(
-          child: SizedBox(
+        return Container(
+          child: Container(
             height: 300,
             width: 250,
-            //height: Curves.easeInOut.transform(value) * 300,
-            //width: Curves.easeInOut.transform(value) * 250,
+            color: Colors.teal,
             child: widget,
           ),
         );
       },
       child: Column(
         children: <Widget>[
-          
           Container(
-            height: 200, width: 400,
+            height: 200,
+            width: 400,
             margin: EdgeInsets.all(10),
             decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(20.0), 
-              border: Border.all(color: Colors.grey[400]),
-              image: DecorationImage(image: AssetImage(images[index]), fit: BoxFit.fill)
-            ), 
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(20.0),
+                border: Border.all(color: Colors.grey[400]),
+                image: DecorationImage(
+                    image: AssetImage(images[index]), fit: BoxFit.fill)),
           ),
           Center(
             child: Text(
               name[index],
               style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'CuteFont'
-              ),
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'CuteFont'),
             ),
           ),
         ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:mynewapp/Screens/Main/Menu/menu.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MaterialApp(
-      //home:Login(),
-      // home:SignUp(),
-      home: Scaffold(
-        body: Menu()
-      ),
-      //home:LandingPage(),
-      debugShowCheckedModeBanner: false,
-    ));
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  statusBarBrightness: Brightness.light
+  ));
+  runApp(MaterialApp(
+    //home:Login(),
+    // home:SignUp(),
+    home: Scaffold(body: Menu()),
+    //home:LandingPage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
