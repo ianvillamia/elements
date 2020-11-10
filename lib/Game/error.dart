f165e89401ace675d38791d2df4a511cb2fbcecb
ianvillamia <ianvillamia0123@gmail.com>
2020-11-11 00:02:13 +0800

    sounds added

--- lib/Game/error.dart
+++ lib/Game/error.dart
@@ -1,4 +1,5 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 import 'package:mynewapp/Utils/textStyles.dart';
@@ -11,52 +12,65 @@ class ErrorScreen extends StatefulWidget {
 }
 
 class _ErrorsState extends State<ErrorScreen> {
+  @override
+  void initState() {
+    super.initState();
+    AudioCache player = new AudioCache();
+    const alarmAudioPath = "GASP.mp3";
+    player.play(alarmAudioPath);
+  }
+
   String _animation = "action";
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        width: size.width,
-        height: size.height,
-        color: Color.fromRGBO(68, 0, 154, 1),
-        child: Stack(children: [
-          FlareActor(
-            "assets/error.flr",
-            alignment: Alignment.center,
-            //fit: BoxFit.contain,
-            animation: _animation,
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              height: size.height * .2,
-              child: Swing(
-                child: Column(
-                  children: [
-                    Text(
-                      'Ooops wrong answer',
-                      style: CustomTextStyles.customText(
-                          color: Colors.white,
-                          isBold: true,
-                          size: FontSizes.large),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      'click to try again',
-                      style: CustomTextStyles.customText(
-                          color: Colors.white,
-                          isBold: true,
-                          size: FontSizes.small),
-                    ),
-                  ],
+      body: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Container(
+          width: size.width,
+          height: size.height,
+          color: Color.fromRGBO(68, 0, 154, 1),
+          child: Stack(children: [
+            FlareActor(
+              "assets/error.flr",
+              alignment: Alignment.center,
+              //fit: BoxFit.contain,
+              animation: _animation,
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: size.height * .2,
+                child: Swing(
+                  child: Column(
+                    children: [
+                      Text(
+                        'Ooops wrong answer',
+                        style: CustomTextStyles.customText(
+                            color: Colors.white,
+                            isBold: true,
+                            size: FontSizes.large),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'click to try again',
+                        style: CustomTextStyles.customText(
+                            color: Colors.white,
+                            isBold: true,
+                            size: FontSizes.small),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ]),
+          ]),
+        ),
       ),
     );
   }
--- lib/Game/success.dart
+++ lib/Game/success.dart
@@ -1,4 +1,5 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 import 'package:mynewapp/Utils/textStyles.dart';
@@ -14,59 +15,73 @@ class Success extends StatefulWidget {
 class _SuccessState extends State<Success> {
   String _animation = "Tadaaah";
   @override
+  void initState() {
+    super.initState();
+    AudioCache player = new AudioCache();
+    const alarmAudioPath = "TADA.mp3";
+    player.play(alarmAudioPath);
+  }
+
+  @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        width: size.width,
-        height: size.height,
-        color: Color.fromRGBO(68, 0, 154, 1),
-        child: Stack(children: [
-          FlareActor(
-            "assets/Tadaaah.flr",
-            alignment: Alignment.center,
-            //fit: BoxFit.contain,
-            animation: _animation,
-          ),
-          NimaActor("assets/robo2",
+      body: GestureDetector(
+        onTap: () {
+          print('tapped');
+          Navigator.pop(context);
+        },
+        child: Container(
+          width: size.width,
+          height: size.height,
+          color: Color.fromRGBO(68, 0, 154, 1),
+          child: Stack(children: [
+            FlareActor(
+              "assets/Tadaaah.flr",
               alignment: Alignment.center,
-              fit: BoxFit.contain,
-              animation: "Flight"),
-          Align(
-            alignment: Alignment.center,
-            child: JelloIn(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'CONGRATS ',
-                    style: CustomTextStyles.customText(
-                        isBold: true,
-                        color: Colors.white,
-                        size: FontSizes.heading),
-                  ),
-                  Text(
-                    "you've got it",
-                    style: CustomTextStyles.customText(
-                        isBold: true,
-                        color: Colors.white,
-                        size: FontSizes.heading),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    "press anywhere to continue",
-                    style: CustomTextStyles.customText(
-                        isBold: true,
-                        color: Colors.white,
-                        size: FontSizes.small),
-                  ),
-                ],
+              //fit: BoxFit.contain,
+              animation: _animation,
+            ),
+            NimaActor("assets/robo2",
+                alignment: Alignment.center,
+                fit: BoxFit.contain,
+                animation: "Flight"),
+            Align(
+              alignment: Alignment.center,
+              child: JelloIn(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'CONGRATS ',
+                      style: CustomTextStyles.customText(
+                          isBold: true,
+                          color: Colors.white,
+                          size: FontSizes.heading),
+                    ),
+                    Text(
+                      "you've got it",
+                      style: CustomTextStyles.customText(
+                          isBold: true,
+                          color: Colors.white,
+                          size: FontSizes.heading),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "press anywhere to continue",
+                      style: CustomTextStyles.customText(
+                          isBold: true,
+                          color: Colors.white,
+                          size: FontSizes.small),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ]),
+          ]),
+        ),
       ),
     );
   }
