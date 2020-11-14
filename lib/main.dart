59ea403f8a794771760e98326069d2a15458f948
ianvillamia <ianvillamia0123@gmail.com>
2020-11-14 13:18:51 +0800

    removed device preview

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,6 @@ import 'package:mynewapp/Services/routing.dart';
 import 'package:mynewapp/Strings/routes.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
-import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
